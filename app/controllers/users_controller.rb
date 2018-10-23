0b6457393ff8532d05edc45934ce7fe07e801db6
Kendall Yee <kendallyee9893@gmail.com>
2018-10-24 00:05:51 +0800

    Bycrpt signup/login done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+def new
+
+		# @user = User.new
+
+	end
+
+	def create
+
+		user = User.new(user_params)
+
+		if user.save
+      session[:user_id] = user.id
+
+			flash[:notice] = "Account successfully registered!"
+			redirect_to '/'
+
+		else
+
+			flash[:alert] = "Unable to register account, check details and retry!"
+			render '/signup'
+
+		end
+
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def user_params
+
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  has_secure_password
+
+end
